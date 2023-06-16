2a34ac361555c3087322064491f7dfb4362a1e0c
Kareem <kareem@gmail.com>
2023-06-15 19:17:02 -0700

    pushing 0x00-hello_world task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, str, 59)
 	return (1);
 }
