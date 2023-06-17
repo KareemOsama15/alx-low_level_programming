3739902bacd3ffa670abd412cf049a4434f947c3
Kareem <kareem@gmail.com>
2023-06-16 18:34:06 -0700

    pushing 0x01-variables_if_else_while task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program prints alphabet in reverse
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
