3d6933e1138e5f01a3fe824e4953bbe6d1ea4481
Kareem <kareem@gmail.com>
2023-06-16 18:06:36 -0700

    pushing 0x01-variables_if_else_while task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints single digit of base 10
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int num, i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", num);
+		i++;
+	}
+	return (0);
+}
