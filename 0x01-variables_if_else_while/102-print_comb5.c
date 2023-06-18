f32f63c484424a2cbca6c7f25adf626f37c9901d
Kareem <kareem@gmail.com>
2023-06-17 17:24:00 -0700

    pushing 0x01-variables_if_else_while task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible combinations of 2 2-digit numbers
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int w = 48, x, y, z;
+
+	while (w < 58)
+	{
+		x = 48;
+		while (x < 58)
+		{
+			putchar(' ');
+			y = 48;
+
+			while (y < 58)
+			{
+				z = 48;
+				while (z < 58)
+				{
+					if (z > y && z > x && z > w && y > x && y > w && x > w)
+					{
+						putchar(w);
+						putchar(x);
+						putchar(y);
+						putchar(z);
+					}
+					if (z == 57 && y == 57 && x == 56 && w == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+
+					z++;
+				}
+				y++;
+			}
+			x++;
+		}
+		w++;
+	}
+
+	putchar('\n');
+	return (0);
+}
