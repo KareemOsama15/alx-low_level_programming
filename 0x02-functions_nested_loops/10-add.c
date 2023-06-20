a663b96a602ec9f492a86f7ab319797ff434db90
Kareem <kareem@gmail.com>
2023-06-20 11:25:54 -0700

    pushing 0x02-function,nested_loops task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @a: parameter contain first number
+ * @b: parameter contain second number
+ *
+ * Return: summation of two numbers
+ */
+int add(int b, int a)
+{
+	return (b + a);
+	_putchar(\n);
+}
