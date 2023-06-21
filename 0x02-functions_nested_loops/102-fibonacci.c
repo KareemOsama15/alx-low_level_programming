475bedd6a85a098c99725be6de6d7c41eb93a36f
Kareem <kareem@gmail.com>
2023-06-21 15:17:33 -0700

    pushing 0x01-variables_if_else_while task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int f1 = 1, f2 = 2, fn, i = 4;
+
+	printf("%d, %d", f1, f2);
+	while (i < 10)
+	{
+		fn = f1 + f2;
+		printf(", %d", fn);
+
+		f1 = f2;
+		f2 = fn;
+		i++;
+	}
+	return (0);
+}
