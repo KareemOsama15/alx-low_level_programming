0921b2323b7dfd83c0a4b23ab3771afca24fb8b9
kareem <kareem@gmail.com>
2023-06-22 21:28:00 +0300

    pushing 0x04-more_functions_nested_loops task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - program draws a straight line
+ * in the terminal.
+ *
+ * @n: parameter contain tests
+ *
+ * Return: Always 0(success)
+ */
+void print_line(int n)
+{
+	int c = 95, i = 1;
+	
+	if (n != 0 && n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar(95);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
