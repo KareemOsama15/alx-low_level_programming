368c02436b32014fd4e4e779bb0045124d59bec0
kareem <kareem@gmail.com>
2023-06-22 18:56:30 +0300

    pushing 0x04-more_functions_nested_loops task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - program that multiplies two integers
+ *
+ *@a: parameter conains number1
+ *@b: paremeter contains number2
+ *
+ * Return: Always 0(success)
+ */
+int mul(int a, int b)
+{
+	return (a + b);
+}
