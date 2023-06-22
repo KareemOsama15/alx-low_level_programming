426f1bafb1dfcb179ed3600ee03396e8c8fd1435
kareem <kareem@gmail.com>
2023-06-22 20:09:08 +0300

    pushing 0x04-more_functions_nested_loops task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,15 @@
  */
 void more_numbers(void)
 {
-	int i, n;
+	int i, ch;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (n = 0; n <= 14; n++)
+		for (ch = 0; ch < 15; ch++)
 		{
-			if (n >= 10)
-			{
-				_putchar((n \ 10) + 48);
-			}
-			_putchar((n % 10) + 48);
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
 		}
 		_putchar('\n');
 	}
