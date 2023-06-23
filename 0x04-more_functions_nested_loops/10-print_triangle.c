6652e0dcceeb2d20d5f645527bd628fd2a6595a6
kareem <kareem@gmail.com>
2023-06-23 03:28:23 +0300

    pushing 0x04-more_functions_nested_loops task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle.
+ *
+ * @size: parameter contain numbers of lines
+ *
+ * Return: no return.
+ */
+void print_triangle(int size)
+{
+	int i, p;
+
+	for (i = 0; i < size; i++)
+	{
+		for (p = 1; p < (size - i); p++)
+			_putchar(' ');
+		for (p--; p < size; p++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
