37e574ee37c9a82286cadcf80f94c20503dd058e
kareem <kareem@gmail.com>
2023-06-25 23:17:30 +0300

    pushing 0x04-more_functions_nested_loops task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,20 +8,18 @@
 int main(void)
 {
 	long int num = 612852475143;
-	unsigned int i, fp, p = 2;
+	unsigned int i, fp = 2;
 
 	for (i = 2; i <= (num / 2); i++)
 	{
-		if (num % i == 0)
+		while (num % i == 0)
 		{
-			while (p <= i)
-			{
-				if (i % p == 0 && p == i)
-					fp = p;
-				p++;
-			}
+			num = num / i;
+
+			if (fp <= i)
+				fp = i;
 		}
-		printf("%d", fp);
 	}
+	printf("%d\n", fp);
 	return (0);
 }
