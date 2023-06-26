49b9200c1e5619d58d1d2147ce97fe559d854149
kareem <kareem@gmail.com>
2023-06-26 23:44:21 +0300

    pushing 0x05-pointers_arrays_strings task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string,
+ *  in reverse
+ *
+ * @s: parameter contain string
+ *
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (i-- >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
