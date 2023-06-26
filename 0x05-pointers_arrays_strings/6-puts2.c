a650a189066fb1c8afa01234dbb9bcdf39cad255
kareem <kareem@gmail.com>
2023-06-27 00:59:20 +0300

    pushing 0x05-pointers_arrays_strings task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -16,9 +16,10 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(str[i]);
-		if (str[i] == ".\0Holberton")
+		if (str[i] == '\0')
 			break;
+
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
