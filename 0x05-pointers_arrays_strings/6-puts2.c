8c2426ab22ae1a29b5594026c65db75cfaf1739c
kareem <kareem@gmail.com>
2023-06-27 00:37:44 +0300

    pushing 0x05-pointers_arrays_strings task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other
+ * character of a string.
+ *
+ * @str: parameter contain character
+ * of a sting
+ *
+ * Return: Always 0(success)
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
