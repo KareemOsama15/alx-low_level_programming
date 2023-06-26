834e4ff5e732e6317ab4a240e323d26ab4adc4f2
kareem <kareem@gmail.com>
2023-06-26 18:29:12 +0300

    pushing 0x05-pointers_arrays_strings task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - program  takes pointer to an int
+ * as parameter and updates the value.
+ *
+ * @n: parameter contain value of 'n'
+ *
+ * Return: Always 0(success)
+ */
+void reset_to_98(int *n)
+{
+    int **p = &n;
+
+    **p = 98;
+
+}
