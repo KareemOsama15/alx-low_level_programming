8363340c9e22a90a8f83298cfe900235543efa2b
kareem <kareem@gmail.com>
2023-06-26 18:11:30 +0300

    pushing 0x05-pointers_arrays_strings task 0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - program
+ *
+ * @a: parameter contain 'a' value
+ * @b: parameter contain 'b' value
+ *
+ * Return: Always 0(success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
