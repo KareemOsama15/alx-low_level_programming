ebf328442c8166aed3988862027a077f6a17d230
kareem <kareem@gmail.com>
2023-06-27 05:14:27 +0300

    pushing 0x05-pointers_arrays_strings task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,17 +10,24 @@
  */
 void rev_string(char *s)
 {
-	int i = 0, n = 0;
-	char temp;
+	int n = 0, i, j;
+	char *str, temp;
 
-	while (s[i] != '\0')
-		++i;
-
-	while (n < i / 2)
+	while (n >= 0)
 	{
-		temp = s[n];
-		s[n] = s[n - 1 - i];
-		s[n - 1 - i] = temp;
-		++n;
+		if (s[n] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
 	}
 }
