7eb2009cf1010dcddb6f05078ef3bcbeee5ef2cf
kareem <kareem@gmail.com>
2023-06-27 03:03:32 +0300

    pushing 0x05-pointers_arrays_strings task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - function
+ *
+ * @a: parameter contain index of array value
+ * @n: parameter contain number of elements
+ * Return: indexes of an array
+ */
+void print_array(int *a, int n)
+{
+	int i = 1;
+
+	printf("%d", a[0]);
+	while (i < n)
+	{
+		printf(", %d", a[i]);
+		i++;
+	}
+}
