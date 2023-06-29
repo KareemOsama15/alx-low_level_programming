f3873c2b94f3d5042d34e3e658f36d69d6d4b463
kareem <kareem@gmail.com>
2023-06-30 01:43:14 +0300

    pushing 0x06-pointers_arrays_strings task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - function that copy string
+ *  for n bytes
+ *
+ * @dest: destination string
+ * @src: source sting
+ * @n: n bytes from src
+ *
+ * Return: dset string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+
+		if (*(src + i) == '\0')
+			break;
+		i++;
+	}
+	return (dest);
+}
