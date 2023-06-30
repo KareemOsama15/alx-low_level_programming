df01b3ec2dfc9015e4aa4f411673326de29aeb6c
kareem <kareem@gmail.com>
2023-06-30 05:09:02 +0300

    pushing 0x06-pointers_arrays_strings task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - function function that encodes
+ * a string into 1337
+ *
+ * @str: parameter contain string
+ *
+ * Return: exchange charactres with numbers
+ */
+char *leet(char *str)
+{
+	int i, e;
+	char upp_case[] = {65, 69, 79, 84, 76};
+	char low_case[] = {97, 101, 111, 116, 108};
+	int num[] = {52, 51, 48, 55, 49};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (e = 0; e < 5; e++)
+		{
+			if (str[i] == num[e])
+				str[i] = num[e];
+			break;
+		}
+	}
+	return (str);
+}
