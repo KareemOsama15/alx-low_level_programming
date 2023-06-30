e1f3a1aea1ebcb7bbc4bda1341b41ed1c41e0b17
kareem <kareem@gmail.com>
2023-06-30 04:53:46 +0300

    pushing 0x06-pointers_arrays_strings task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,25 +11,24 @@
  */
 char *cap_string(char *s)
 {
-	int i, e = 0;
-	char sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (s[i] >= 97 && s[i] <= 122)
-		s[i] -= 32;
-	i++;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		while (e < 13)
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i] == sep[e])
+			if (*(s + count) == sep_words[i])
 			{
-				if (s[i] >= 97 && s[i] <= 122)
-					*(s + (i + 1)) = *(s + (i + 1)) - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
-			e++;
 		}
+		count++;
 	}
 	return (s);
 }
