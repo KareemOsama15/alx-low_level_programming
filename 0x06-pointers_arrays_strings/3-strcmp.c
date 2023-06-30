aaf60894139e3a0bc638eac66cc5da02ca4f444f
kareem <kareem@gmail.com>
2023-06-30 17:16:20 +0300

    pushing 0x06-pointers_arrays_strings task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,21 +13,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	/* 2 loops - divide s1 index from s2 index */
-	/* - get (0) if equal - (-1) if s1 < s2 - (1) if s1 > s2 */
+	int i = 0, res = 0;
 
-	int i, c;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while (res == 0)
 	{
-		for (c = 0; s2[i] != '\0'; c++)
-		{
-			if (s1[i] - s2[c] == 0)
-				return (0);
-			else if (s1[i] - s2[c] < 0)
-				return (s1[i] - s2[c]);
-			else
-				return (s1[i] - s2[c]);
-		}
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		res = s1[i] - s2[i];
+		i++;
 	}
+	return (res);
 }
