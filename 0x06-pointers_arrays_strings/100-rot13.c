f0fd66e087fca2c653eaf288a59299fd59e7b50b
kareem <kareem@gmail.com>
2023-07-01 05:30:05 +0300

    pushing 0x06-pointers_arrays_strings task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - function that encodes
+ * a string using rot13.
+ *
+ * @str: parameter contain string
+ *
+ * Return: exchange charactres with numbers
+ */
+char *rot13(char *str)
+{
+	/*-return leters and numbers to ascii */
+	/* - 1 if to check if index is a chararacter capital or small  */
+	/*- 2 loops ,one for continue until reach to \0 */
+	/* ,second to change index of char with ceaser code  */
+
+	char alphabet[] = {ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz};
+	char ceaser[] = {NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm};
+	int i, e;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (e = 0; e < 52 ; e++)
+		{
+			if (str[i] == alphabet[e])
+				str[i] = ceaser[e];
+		}
+	}
+	return (str);
+}
