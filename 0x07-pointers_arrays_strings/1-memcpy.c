ed437d60f9aa551beb8ee519f327810e47f5c20b
kareem <kareem@gmail.com>
2023-07-03 14:58:40 +0300

    pushing 0x07-pointers_arrays_strings task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: copy string
+ * @src: string in memory area
+ * @n: bytes
+ * Return: Always 0(success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
