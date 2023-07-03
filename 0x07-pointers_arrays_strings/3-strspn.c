df9c996e174dc1f18215ca8abc26bb95c5b08737
kareem <kareem@gmail.com>
2023-07-03 18:07:18 +0300

    pushing 0x07-pointers_arrays_strings task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	/* loop on s to get index and go for acceot*/
+	/* loop on s to get index and go for accept*/
 	/*and loop on it to found uf the char exist or not*/
 	/*and there is 'op = 0' if return to 1 so there is char found*/
 	/*check from second loop and don't check from first loop */
