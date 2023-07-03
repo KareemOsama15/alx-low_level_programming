dcfe6f7b400905a70cc29f4e16adc483116375f8
kareem <kareem@gmail.com>
2023-07-03 14:27:57 +0300

    pushing 0x07-pointers_arrays_strings task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  _memset - function hat fills memory with
+ *   a constant byte
+ *
+ * @s: pointer to the momery area
+ * @b: constant byte
+ * @n: bytes of memory area
+ *
+ * Return: pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+		*(s + i) = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# This is readme file for 0x07-pointers_arrays_strings
