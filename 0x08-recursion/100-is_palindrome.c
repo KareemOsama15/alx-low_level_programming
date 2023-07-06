4b0ad5f510545f83953a65340bc17e58e2301820
kareem <kareem@gmail.com>
2023-07-06 19:16:52 +0300

    pushing 0x08-recursion task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,9 +26,9 @@ int is_palindrome(char *s)
  */
 int palind_check(char *s, int i)
 {
-	if (*(s + i) == *(s + str_len(s - 1 - i)) && i < str_len(s) \ 2)
+	if (*(s + i) == *(s + str_len(s - 1 - i)) && i < str_len(s) / 2)
 		return (1);
-	if (*(s + i) != *(s + str_len(s - 1 - i)) && i < str_len(s) \ 2)
+	if (*(s + i) != *(s + str_len(s - 1 - i)) && i < str_len(s) / 2)
 		return (0);
 	else
 		return (palind_check(s, i));
