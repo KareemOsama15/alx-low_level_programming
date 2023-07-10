504dd11c824622492ca30da6dcfa809c8f8c613d
kareem <kareem@gmail.com>
2023-07-10 23:03:08 +0300

    pushing 0x0B-malloc_free task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - function that creates an array
+ *	 of chars,initializes it with a specific char
+ *
+ * @c:arguement of character
+ * @size: arguemet of size
+ * Return: string of character
+ */
+char *create_array(unsigned int size, char c)
+{
+	/*if size is 0 return null*/
+	if (size == 0)
+		return (NULL);
+
+	/*declare pointer to allocate memory in the heap*/
+	char *ch = malloc(size * sizeof(c));
+
+	/*return null if pointer of allocating memory fail*/
+	if (ch == NULL)
+		return (NULL);
+
+	/*fullfil allocated memory with value of parameter c passed to func*/
+	for (int i = 0; i < size; i++)
+		ch[i] = c;
+
+	return (ch);
+}
