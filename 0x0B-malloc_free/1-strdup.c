afdce9d7a9ebd4c950b78920f2844a4d8172d4ff
kareem <kareem@gmail.com>
2023-07-11 01:17:47 +0300

    pushing 0x0B-malloc_free task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - that returns a pointer to a newly 
+ *  allocated space in memory
+ *  ,copy string given as a parameter
+ *
+ * @str:arguement of string
+ *
+ * Return: pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	int i, length = 0;
+	char *c_str;
+	
+	if (str == NULL)
+		return (NULL);
+	
+	while (str[length] != '\0')
+		length++;
+	
+	c_str = (char *)malloc*(length * sizeof(char));
+	
+	if (c_str == NULL)
+	{
+		free(c_str);
+		return (NULL);
+	}
+	
+	for (i = 0; i < length; i++)
+		c_str[i] = str[i];
+	
+	return (c_str);
+}
