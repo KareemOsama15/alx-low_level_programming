fa0eff894f3e29953698b8b6dbaf91c4b0e8b0b2
kareem <kareem@gmail.com>
2023-07-11 19:53:05 +0300

    pushing 0x0B-malloc_free task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - function that frees a 2 dimensional
+ *  grid created by your alloc_grid function
+ *
+ * @grid: pointer of pointer of 2 dimensional array
+ * @height: arguemet of array height
+ * Return: no thing
+ */
+void free_grid(int **grid, int height)
+{
+	for (int i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
