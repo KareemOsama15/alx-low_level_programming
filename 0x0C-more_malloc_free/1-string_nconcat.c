d6148dddf1ade08c331cc1f6e6c5abe8bb4b0d32
kareem <kareem@gmail.com>
2023-07-13 19:05:25 +0300

    pushing 0x0C-more_malloc_free task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	/*fullfil allocated memory with its value of 2 passed strings*/
-	for (i = 0; i < (len1 + 1); i++)
+	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
 		str[i] = s2[j];
