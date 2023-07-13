de7168f6c823468898199d34839b6ada9b9de0bb
kareem <kareem@gmail.com>
2023-07-13 19:06:12 +0300

    pushing 0x0C-more_malloc_free task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
-	
+
 	/*condition if s1 or s2 was null treat them like empty string*/
 	if (s1 == NULL)
 		s1 = "";
