9d891e0163c5df6a473ec9cd914d6cd41ff15127
kareem <kareem@gmail.com>
2023-07-13 17:12:25 +0300

    pushing 0x0B-malloc_free task 6

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - function that concatenates 
+ *  all the arguments of your program.
+ *
+ * @ac: arguement of count
+ * @av: arguemet of vector
+ * Return: string followed by new line
+ */
+char *argstostr(int ac, char **av)
+{
+    
+}
