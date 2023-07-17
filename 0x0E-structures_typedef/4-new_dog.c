c5c175993e18a9b43650a92e18f5e0f90cbbebde
kareem <kareem@gmail.com>
2023-07-18 02:24:17 +0300

    pushing 0x0E-structures_typedef task 5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - function that creates a new dog.
+ * @name: pointer of character
+ * @age: integer
+ * @owner: pointer of character
+ *
+ * Return: nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if ()
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs.
+ * @d: pointer of struct dog
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+}
