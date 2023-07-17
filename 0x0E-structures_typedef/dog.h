37b8fe311f698d12a7ca780e1ff6d0befa46d952
kareem <kareem@gmail.com>
2023-07-18 00:14:42 +0300

    pushing 0x0E-structures_typedef task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/*
+ * struct dog - structure contain some 
+ *  information about dog
+ *
+ * @name: name of the dog
+ * @age: the age of the dog 
+ * @owner: the owner of the dog
+ *
+ */
 struct dog 
 {
     char *name;
