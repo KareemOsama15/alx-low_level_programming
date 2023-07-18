ec0bb4a3a42eeb30dd5e2987fc8a255ba0c876d0
kareem <kareem@gmail.com>
2023-07-18 03:31:15 +0300

    pushing 0x0E-structures_typedef task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,24 +17,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	/*allocate memory for pointer point to a different data of a structure type*/
 	ptr_dog = malloc(sizeof(*ptr_dog));
-
-	/*if this pointer or any of its data type equal null free them and return null*/
+	/*if this pointer or any of its data type equal null free them, return null*/
 	if (ptr_dog == NULL || !(name) || !(owner))
 	{
-		free (ptr_dog);
+		free(ptr_dog);
 		return (NULL);
 	}
-
 	/*get length of strings owner and name*/
 	for (len_name = 0; name[len_name] != '\0'; len_name++)
 		;
 	for (len_own = 0; owner[len_own] != '\0'; len_own++)
 		;
-
 	/*allocate memory to copy a specific length for a struct data type*/
 	ptr_dog->name = malloc(len_name + 1);
 	ptr_dog->owner = malloc(len_own + 1);
-
 	if (!(ptr_dog->name) || !(ptr_dog->owner))
 	{
 		free(ptr_dog->name);
@@ -42,7 +38,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ptr_dog);
 		return (NULL);
 	}
-
 	/*copy passed arguements to the memory allocated bytes*/
 	for (i = 0; i < len_name; i++)
 		ptr_dog->name[i] = name[i];
@@ -50,7 +45,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	ptr_dog->age = age;
 
-	for (i = 0; i< len_own; i++)
+	for (i = 0; i < len_own; i++)
 		ptr_dog->owner[i] = owner[i];
 	ptr_dog->owner[i] = '\0';
 
