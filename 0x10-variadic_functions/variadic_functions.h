7ab5392ce0de28925b86d32726236a892ab0a90f
kareem <kareem@gmail.com>
2023-07-21 01:16:56 +0300

    pushing 0x10-variadic_functions task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
