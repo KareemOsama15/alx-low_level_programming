ef8a880108183fcbba1ec2f103ae17e14803bc69
kareem <kareem@gmail.com>
2023-07-20 19:44:48 +0300

    pushing 0x0F-function_pointers task 4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,26 +11,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int *ptr;
+	int num1, num2;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	ptr = get_op_func(argv[2]);
-	if (!(ptr))
+
+	if (argv[2][1])
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	result = get_op_func(argv[2])(num1, num2);
-	printf("%d\n", result);
-
+	printf("%d\n", operation(num1, num2));
 	return (0);
 }
