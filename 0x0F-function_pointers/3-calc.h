22c052f23c84d7caac400677f95c9f611fe27349
kareem <kareem@gmail.com>
2023-07-20 17:09:37 +0300

    pushing 0x0F-function_pointers task 4

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,8 @@
 #ifndef _CALC_H_
 #define _CALC_H_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
 * struct op - stucture tag name
@@ -17,7 +17,6 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
