fd90d6ee7bb65d8a0ff4907e50953fb5dbf10011
kareem <kareem@gmail.com>
2023-07-21 04:28:40 +0300

    pushing 0x10-variadic_functions task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,7 +31,7 @@ void print_all(const char * const format, ...)
 					break;
 			case 's':
 					str = va_arg(ptr, char *), x = 1;
-					if (!(str))
+					if (!str)
 					{
 						printf("(nil)");
 						break;
