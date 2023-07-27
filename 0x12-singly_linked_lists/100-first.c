0cb442e4b0ad9eb47e593362c9b105a86d78b21e
Kareem <kareem@gmail.com>
2023-07-28 02:57:40 +0300

    pushing 0x12-singly_linked_lists task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * before_main - function that excute before main.
+ *
+ * Return: no return
+ */
+void before_main() __attribute__((constructor))
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
