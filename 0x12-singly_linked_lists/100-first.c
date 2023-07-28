c66accfe5d766e64bdb3267ed0de9af1e14a80fa
Kareem <kareem@gmail.com>
2023-07-28 03:06:34 +0300

    pushing 0x12-singly_linked_lists task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,7 @@
  *
  * Return: no return
  */
-void before_main(void) __attribute__((constructor))
+void __attribute__ ((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
