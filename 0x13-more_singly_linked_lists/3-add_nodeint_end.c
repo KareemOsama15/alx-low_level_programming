2ff44b6429c2f0dbaa3421e72bf89ebd36255e34
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-07-31 22:52:48 +0300

    pushing 0x13-more_singly_linked_lists [fix] task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,7 +24,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new;
 	else
 	{
-		while (temp->next)
+		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new;
 	}
