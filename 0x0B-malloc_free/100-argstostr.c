4b1346e4f638b3159febcc8b3ce561d272a45925
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-08-03 01:03:36 +0300

    pushing 0x00x0B-malloc_freesk 6

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,5 +11,31 @@
  */
 char *argstostr(int ac, char **av)
 {
-    
+	int i, j, e = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+	len += ac;
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[e] = av[i][j];
+			e++;
+		}
+		if (str[e] == '\0')
+			str[e++] = '\n';
+	}
+	return (str);
 }
