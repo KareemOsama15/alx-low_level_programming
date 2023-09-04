b7cb96043c42a81b7d6993b49ac6fa5cf2e6ee01
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-09-04 22:23:45 +0300

    pushing 0x15-file_io project tasks 0, 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		text_content = "";
-	for (len = 0; *text_content; len++)
-		text_content++;
+	for (len = 0; text_content[len]; len++)
+		;
 
 	n_write = write(fd, text_content, len);
-	if (n_write == -1)
+	if (n_write != len)
 		return (-1);
 
 	close(fd);
