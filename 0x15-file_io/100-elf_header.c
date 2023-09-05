eec324b88b375b858a203b60b6147fa1de65e0c8
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-09-05 21:36:37 +0300

    pushing 0x15-file_io project task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * check_elf - check if it is elf file or not
+ *
+ * @ptr: magic.
+ * Return: 1 if ELF file, 0 if not
+ */
+int check_elf(char *ptr)
+{
+	int add = (int)ptr[0];
+	char E = ptr[1];
+	char L = ptr[2];
+	char F = ptr[3];
+
+	if (add == 127 && E == 'E' && L == 'L' && F == 'F')
+		return (1);
+
+	return (0);
+}
+
+/**
  * main - check the code for Holberton School students.
  * @argc: number of arguments.
  * @argv: arguments vector.
@@ -10,7 +29,7 @@ int main(int argc, char **argv)
 {
 	/*Task not completed*/
 	int fd, n_read;
-	char buffer[27];
+	char ptr[27];
 
 	if (argc != 2)
 	{
@@ -25,10 +44,16 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	n_read = read(fd, buffer, 27);
+	n_read = read(fd, ptr, 27);
 	if (n_read == -1)
 	{
 		dprintf(STDERR_FILENO, "File can't be read\n");
+		exit(98);
+	}
+
+	if (check_elf(ptr) == 0)
+	{
+		dprintf(STDERR_FILENO, "File is not an ELF file\n");
 		exit(98);
 	}
 
