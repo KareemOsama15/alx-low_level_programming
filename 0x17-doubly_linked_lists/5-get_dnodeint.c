84e9d1dd6a44597260ef6a91d06ab1afd333514e
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-12 18:34:04 +0300

    [push]  project 0x17-doubly_linked_lists task[5]

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,8 +11,9 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp = head;
+	size_t length = dlistint_len(head) - 1;
 
-	if (head == NULL)
+	if (head == NULL || index > length)
 		return (NULL);
 	else if (index == 0)
 		return (head);
@@ -20,8 +21,32 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (index != 0)
 	{
 		/* 10 15 20 30 40   idx = 4*/
+		if (temp == NULL)
+        	return (NULL);
 		temp = temp->next;
 		index--;
 	}
 	return (temp);
+}
+
+/**
+ * dlistint_len - function returns number
+ *   of linked list nodes
+ *
+ * @h: pointer to first node
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	if (!h)
+		return (count);
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
 }
