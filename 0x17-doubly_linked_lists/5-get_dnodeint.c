0ecf06c4c53098636aab666faedede238ed2ed3b
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-10-12 18:35:11 +0300

    [push]  project 0x17-doubly_linked_lists task[5]

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		/* 10 15 20 30 40   idx = 4*/
 		if (temp == NULL)
-        	return (NULL);
+			return (NULL);
 		temp = temp->next;
 		index--;
 	}
