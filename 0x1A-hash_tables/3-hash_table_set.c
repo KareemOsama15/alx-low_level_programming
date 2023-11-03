5cbc23d05d57cb743e4cb4dc6c7b44c13e2d3b85
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-11-03 20:02:14 +0200

    [fix] project 0x1A-hash_tables tasks[3]

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned int i;
 	hash_node_t *newNode = NULL, *temp = NULL;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0)
+	if (ht == NULL || key == NULL || *key ==  '\0' || strlen(key) == 0)
 		return (0);
 
 	i = key_index((unsigned char *)key, ht->size);
